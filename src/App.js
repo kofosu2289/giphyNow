f7637653a976141b10a5db51838448e607e62b09
Kenneth Ofosu <kofosu89@gmail.com>
2019-06-29 19:00:32 -0400

    log search results to dev console

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
 import { PageHeader, Tabs, Tab } from 'react-bootstrap';
+import './App.css';
 
 import Search from './Search';
 import Functionalities from './Functionalities';
 
-import './App.css';
+var GphApiClient = require('giphy-js-sdk-core')
 
 class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            giphy: GphApiClient("8XADJBZWvzB75qIDyCpfWLbnE5otD7wG"),
+            searchQuery: ""
+        };
+
+       
+    }
+
+    search = async (e) => {
+        e.preventDefault();
+
+        await this.state.giphy.search('gifs', { "q": this.state.searchQuery })
+            .then((response) => {
+                response.data.forEach((gifObject) => {
+                    console.log(gifObject);
+                })
+            })
+            .catch((err) => {
+                
+            });
+    }
+
+    updateQuery = (e) => {
+        this.setState({
+            searchQuery: e.target.value
+        });
+    }
 
     render() {
         return (
@@ -14,8 +45,12 @@ class App extends Component {
                 <PageHeader>
                     giphyNow
                 </PageHeader>
-                <Search />
-                <Functionalities />
+            <Search
+              query={this.state.searchQuery}
+              search={this.search}
+              handleChange={this.updateQuery}
+            />
+            <Functionalities />
             </section>
         );
     }
--- src/Search.js
+++ src/Search.js
@@ -2,25 +2,35 @@ import React, { Component } from 'react';
 import { Navbar, FormGroup, FormControl, Button } from 'react-bootstrap';
 
 class Search extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <Navbar>
-                <Navbar.Header>
-                    <Navbar.Brand>
-                        <a href="#home">Discover: </a>
-                    </Navbar.Brand>
-                    <Navbar.Toggle />
-                </Navbar.Header>
-                <Navbar.Collapse>
-                    <Navbar.Form pullLeft>
-                        <FormGroup>
-                            <FormControl type="text" placeholder="Search" />
-                        </FormGroup>{' '}
-                        <Button type="submit">Search</Button>
-                    </Navbar.Form>
-                </Navbar.Collapse>
-            </Navbar>
+  render() {
+    return (
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#home">Discover: </a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <form onSubmit={this.props.search}>
+            <Navbar.Form pullLeft>
+              <FormGroup>
+                <FormControl
+                  type="text"
+                  placeholder="Search"
+                  value={this.props.query}
+                  onChange={this.props.handleChange}
+                />
+              </FormGroup>{' '}
+              <Button type="submit">Search</Button>
+            </Navbar.Form>
+          </form>
+        </Navbar.Collapse>
+      </Navbar>
         );
     }
 }
